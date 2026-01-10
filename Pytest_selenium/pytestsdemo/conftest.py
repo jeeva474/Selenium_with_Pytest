83cfd6208a927aa1835e1b68cb91c722a0b38b84
jeeva474 <jeeva474@gmail.com>
2026-01-10 22:06:37 +0530

    Update conftest.py

--- Pytest_selenium/pytestsdemo/conftest.py
+++ Pytest_selenium/pytestsdemo/conftest.py
@@ -1,6 +1,5 @@
 import os
 import pytest
-import pytest_html
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 
@@ -15,54 +14,49 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="function")
-def setup(request):
+def driver(request):
     browser_name = request.config.getoption("browser_name")
-    global driver
+
     if browser_name == "chrome":
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--ignore-certificate-errors")
-        chrome_options.add_argument("--disable-notifications")
-        driver = webdriver.Chrome(options=chrome_options)
-    if browser_name == "Firefox":
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-    if browser_name == "edge":
-        service_obj = Service("/Pytest_selenium/selenium_scripts/msedgedriver.exe")
-        driver = webdriver.Edge(service=service_obj)
-        driver.maximize_window()
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-notifications")
+        driver = webdriver.Chrome(options=options)
+
+    elif browser_name.lower() == "firefox":
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}")
 
     yield driver
     driver.quit()
 
-@pytest.hookimpl( hookwrapper=True )
-def pytest_runtest_makereport(item):
-    """
-        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-        :param item:
-        """
-    Pytest_html = item.config.pluginmanager.getplugin('html')
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
     outcome = yield
-    report = outcome.get_result()
-    extra = getattr( report, 'extra', [] )
+    rep = outcome.get_result()
 
-    if report.when == 'call' or report.when == "setup":
-        xfail = hasattr( report, 'wasxfail' )
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            reports_dir = os.path.join( os.path.dirname( __file__ ), 'reports' )
-            file_name = os.path.join( reports_dir, report.nodeid.replace( "::", "_" ) + ".png" )
-            print( "file name is " + file_name )
-            _capture_screenshot( file_name )
-            if file_name:
-                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
-                extra.append(Pytest_html.extras.html(html))
-        report.extras = extra
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver", None)
+        if driver:
+            screenshot_dir = "reports/screenshots"
+            os.makedirs(screenshot_dir, exist_ok=True)
+            file_name = os.path.join(
+                screenshot_dir, f"{item.name}.png"
+            )
+            driver.save_screenshot(file_name)
 
 
 def _capture_screenshot(file_name):
-    driver.get_screenshot_as_file(file_name)
+    driver = globals().get("driver")
+    if driver:
+        driver.get_screenshot_as_file(file_name)
 
 def pytest_html_report_title(report):
     report.title = "Automation report"
@@ -76,5 +70,3 @@ def dataload():
 @pytest.fixture(params=["Google", "FireFox", "IE"])
 def crossbrowser(request):
     return request.param
-
-
