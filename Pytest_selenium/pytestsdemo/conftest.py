89af9346870b795c48479c9fcf92a71a70e9902c
jeeva474 <jeeva474@gmail.com>
2026-01-10 21:49:28 +0530

    Update conftest.py

--- Pytest_selenium/pytestsdemo/conftest.py
+++ Pytest_selenium/pytestsdemo/conftest.py
@@ -20,6 +20,7 @@ def setup(request):
     global driver
     if browser_name == "chrome":
         chrome_options = webdriver.ChromeOptions()
+        chrome_optiona.add_argument("--headless")
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--ignore-certificate-errors")
         chrome_options.add_argument("--disable-notifications")
@@ -75,3 +76,4 @@ def dataload():
 @pytest.fixture(params=["Google", "FireFox", "IE"])
 def crossbrowser(request):
     return request.param
+
